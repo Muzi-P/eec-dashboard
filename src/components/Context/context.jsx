24a3a4d9e212a49de81b305c45562347580653a7
Muzi-P <phiwokuhlegondwe@gmail.com>
2021-04-01 13:15:38 +0800

    Added Maguduza scheduling *problem:error is calculations

--- src/components/Context/context.jsx
+++ src/components/Context/context.jsx
@@ -849,6 +849,7 @@ class InflowsProvider extends Component {
         );
       }
     }
+
     this.calcSum();
   };
   /**
@@ -1406,8 +1407,116 @@ class InflowsProvider extends Component {
     }
     await this.setState({ currentSchedule: generatedSchedule });
     console.log(`Maguga ===========end======`);
+    // maguduza genarations
+    this.populateMaguduza();
   };
 
+  populateMaguduza = async () => {
+    let generatedSchedule = this.state.currentSchedule;
+    let availableWater = 0;
+    const maguduzaRatedFlow = parseFloat(
+      this.state.maguduzaPS.Genarators[0].Rated_Flow
+    );
+    // maguduza can run at 5.6, 5, 4, 3 levels
+
+    // const levelOneHourlyWater = this.calWaterConsumedByAnyMachine(
+    //   1,
+    //   5.6,
+    //   maguduzaRatedFlow
+    // );
+    // const levelTwoHourlyWater = this.calWaterConsumedByAnyMachine(
+    //   1,
+    //   5,
+    //   maguduzaRatedFlow
+    // );
+    // const levelThreeHourlyWater = this.calWaterConsumedByAnyMachine(
+    //   1,
+    //   4,
+    //   maguduzaRatedFlow
+    // );
+    // const levelFourHourlyWater = this.calWaterConsumedByAnyMachine(
+    //   1,
+    //   3,
+    //   maguduzaRatedFlow
+    // );
+    console.log(`Maguduza ===========start======`);
+    generatedSchedule.forEach((item) => {
+      const edwaleniPower = parseFloat(item.EDWALENI);
+      if (item.Time && edwaleniPower > 0) {
+        availableWater = this.edwaleniHourlyOutFlow(edwaleniPower);
+        console.log(
+          ` water release from edwaleni @${edwaleniPower} MW: ${availableWater} during ${item.Time} period`
+        );
+        // console.log(item.EDWALENI);
+        item.MAGUDUZA = this.calcMagugaHourGen(
+          maguduzaRatedFlow,
+          [5.6, 5, 4, 3],
+          availableWater
+        );
+      }
+    });
+    await this.setState({ currentSchedule: generatedSchedule });
+    console.log(`Maguduza ===========end======`);
+  };
+  calcMagugaHourGen = (maguduzaRatedFlow, maguduzaLevels, availableWater) => {
+    let maguduzaHourlyPower = 0;
+    for (let power = 0; power < maguduzaLevels.length; power++) {
+      let waterNeeded = this.calWaterConsumedByAnyMachine(
+        1,
+        maguduzaLevels[power],
+        maguduzaRatedFlow
+      );
+      console.log(
+        `water needed to run maguduza @ ${maguduzaLevels[power]}: ${waterNeeded}`
+      );
+      if (availableWater >= waterNeeded) {
+        maguduzaHourlyPower = maguduzaLevels[power];
+        break;
+      }
+    }
+    console.log(`maguga power ${maguduzaHourlyPower}`);
+    return maguduzaHourlyPower;
+  };
+  edwaleniHourlyOutFlow = (edwaleniPower) => {
+    let hourlyWaterAvailabeForMaguduza = 0;
+    const ratedFlowSmall = parseFloat(
+      this.state.edwaleniPS.Genarators[0].Rated_Flow
+    );
+    const ratedFlowBig = parseFloat(
+      this.state.edwaleniPS.Genarators[1].Rated_Flow
+    );
+    if (edwaleniPower > 5) {
+      hourlyWaterAvailabeForMaguduza = this.calWaterConsumedByAnyMachine(
+        1,
+        5,
+        ratedFlowBig
+      );
+      // console.log("5mw", hourlyWaterAvailabeForMaguduza);
+      hourlyWaterAvailabeForMaguduza += this.calWaterConsumedByAnyMachine(
+        1,
+        edwaleniPower - 5,
+        ratedFlowSmall
+      );
+      // console.log(`${edwaleniPower - 5}`, hourlyWaterAvailabeForMaguduza);
+    }
+
+    if (edwaleniPower <= 5 && edwaleniPower >= 3) {
+      hourlyWaterAvailabeForMaguduza = this.calWaterConsumedByAnyMachine(
+        1,
+        edwaleniPower,
+        ratedFlowBig
+      );
+    }
+
+    if (edwaleniPower < 3) {
+      hourlyWaterAvailabeForMaguduza = this.calWaterConsumedByAnyMachine(
+        1,
+        edwaleniPower,
+        ratedFlowSmall
+      );
+    }
+    return hourlyWaterAvailabeForMaguduza;
+  };
   /**
    * Calculate the volume of irrigation water that will be discharged from the regulating weir in 24hrs.
    * @param {*} Irrigation_Flow irrigation flow (m3/s)
